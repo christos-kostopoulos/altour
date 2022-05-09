f590fa94c835a8499acdebede00e9f991e2c3314
kostop19 <kostop19@gmail.com>
2022-05-09 22:55:17 +0300

    upcoming rolls

---
+++ src/components/UpcomingRoll/UpcomingRoll.js
@@ -0,0 +1,73 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { graphql, StaticQuery } from "gatsby";
+import UpcomingRollItem from "./UpcomingRollItem";
+import "./upcoming.css";
+
+class UpcomingRollTemplate extends React.Component {
+  render() {
+    const { data } = this.props;
+    const { edges: posts } = data.allMarkdownRemark;
+
+    return (
+      <div className="columns is-multiline" style={{ position: "relative", zIndex: '15' }}>
+        {posts &&
+          posts.map(({ node: post }) => (
+            <UpcomingRollItem key={post.id} {...post} />
+          ))}
+      </div>
+    );
+  }
+}
+
+UpcomingRoll.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array,
+    }),
+  }),
+};
+
+export default function UpcomingRoll() {
+  return (
+    <StaticQuery
+      query={graphql`
+        query UpcomingRollQuery {
+          allMarkdownRemark(
+            sort: { order: DESC, fields: [frontmatter___date] }
+            filter: { frontmatter: { templateKey: { eq: "upcoming-event" } } }
+          ) {
+            edges {
+              node {
+                excerpt(pruneLength: 400)
+                id
+                fields {
+                  slug
+                }
+                frontmatter {
+                  title
+                  description
+                  templateKey
+                  date(formatString: "MMMM DD, YYYY")
+                  featuredpost
+                  featuredimage {
+                    childImageSharp {
+                      gatsbyImageData(
+                        height: 500
+                        quality: 100
+                        layout: CONSTRAINED
+                      )
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={(data, count) => (
+        <UpcomingRollTemplate data={data} count={count} />
+      )}
+    />
+  );
+}
---
+++ src/components/UpcomingRoll/UpcomingRollItem.js
@@ -0,0 +1,39 @@
+import React from "react";
+import PreviewCompatibleImage from "../PreviewCompatibleImage";
+import { Link } from "gatsby";
+
+const UpcomingRollItem = (post) => (
+  <div className="column is-4" key={post.id}>
+    <div
+      className="upcoming-card p-3 has-text-centered"
+      style={{ backgroundColor: "#237da5"}}
+    >
+      <header>
+        <h3 className="has-text-white is-size-3">{post.frontmatter.title}</h3>
+        <p className="has-text-white has-text-weight-light pb-2">
+          {post.frontmatter.description}
+        </p>
+      </header>
+      <div className="upcoming-card-image">
+        {post.frontmatter.featuredimage ? (
+          <div className="featured-thumbnail">
+            <PreviewCompatibleImage
+              imageInfo={{
+                image: post.frontmatter.featuredimage,
+                alt: `featured image thumbnail for post ${post.frontmatter.title}`,
+                width:
+                  post.frontmatter.featuredimage.childImageSharp.gatsbyImageData
+                    .width,
+                height:
+                  post.frontmatter.featuredimage.childImageSharp.gatsbyImageData
+                    .height,
+              }}
+            />
+          </div>
+        ) : null}
+      </div>
+    </div>
+  </div>
+);
+
+export default UpcomingRollItem;
